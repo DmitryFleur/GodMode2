aea526932af1208c4e12a7d246f0e948499b26b4
vas3k <me@vas3k.ru>
2016-11-02 13:02:02 +0200

    Added clickers

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import settings
 from db.vas3kru import Vas3kDatabase
 from godmode import GodModeApp
 from models.index import IndexAdminModel
+from models.vas3k_clickers import ClickersAdminModel
 from models.vas3k_comments import CommentAdminModel
 from models.vas3k_memories import MemoryAdminModel
 from models.vas3k_stories import StoryAdminModel
@@ -12,7 +13,8 @@ app = GodModeApp(
         IndexAdminModel,
         StoryAdminModel,
         MemoryAdminModel,
-        CommentAdminModel
+        CommentAdminModel,
+        ClickersAdminModel
     ]
 )
 
