aec946ef33f6069ce51f2d614fb22746ced8e74c
vas3k <me@vas3k.ru>
2016-11-11 00:32:16 +0200

    365 project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import settings
 from db.vas3kru import Vas3kDatabase
 from godmode import GodModeApp
 from models.index import IndexAdminModel
+from models.vas3k_365 import The365AdminModel
 from models.vas3k_clickers import ClickersAdminModel
 from models.vas3k_comments import CommentAdminModel
 from models.vas3k_memories import MemoryAdminModel
@@ -14,7 +15,8 @@ app = GodModeApp(
         StoryAdminModel,
         MemoryAdminModel,
         CommentAdminModel,
-        ClickersAdminModel
+        ClickersAdminModel,
+        The365AdminModel
     ]
 )
 
