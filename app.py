11ea08ec144987b35b852d71e703171fb98658aa
vas3k <me@vas3k.ru>
2017-10-03 17:38:37 +0300

    Posting pictures to channel

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from db.vas3kru import Vas3kDatabase
 from godmode import GodModeApp
 from models.index import IndexAdminModel
 from models.vas3k_365 import The365AdminModel
-from models.vas3k_announce import AnnounceAdminModel
 from models.vas3k_clickers import ClickersAdminModel
 from models.vas3k_comments import CommentAdminModel
 from models.vas3k_memories import MemoryAdminModel
@@ -18,7 +17,6 @@ app = GodModeApp(
         CommentAdminModel,
         ClickersAdminModel,
         The365AdminModel,
-        AnnounceAdminModel
     ]
 )
 
